3f1e6fd166ebc1db458c8306d219fd694eea7b2e
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2025-11-01 16:26:28 +0530

    code changs.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.post("/hello")
-async def webhook(request):
+async def webhook(request : Request):
     data = await request.json()
     print(data)
     return "Hello There, from fastAPI"
