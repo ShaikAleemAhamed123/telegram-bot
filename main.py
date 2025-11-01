02e6b08562d786db0ffcea72b44bf40c0918be69
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2025-11-01 15:49:16 +0530

    webhook for tele bot

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from telegram.ext import Application, CommandHandler
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-async def reply(update, context):
-    await update.message.reply_text("What the hell man ?")
+app = FastAPI()
 
-
-def main():
-    """
-    Handles the initial launch of the program (entry point).
-    """
-    token : str = "8311901559:AAEJIF3HbxtkVnf8YjVYQ5IBgAfcPl9Axh4"
-    application : Application = Application.builder().token(token).concurrent_updates(True).read_timeout(30).write_timeout(30).build()
-    application.add_handler(CommandHandler("hello", reply))
-    print("Telegram Bot started!", flush=True)
-    application.run_polling()
-
-if __name__ == '__main__':
-    main()
+@app.get("/hello")
+async def helloCommandHandler():
+    return {"message": "Hello, from FastAPI!"}
