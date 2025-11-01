9f6df7a9d397913dcbe5d50dd38090a4c775365f
ShaikAleemAhamed123 <shaikaleemahamed786@gmail.com>
2025-11-01 16:42:46 +0530

    code changes

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI, Request
+import httpx
 
 app = FastAPI()
+BOT_TOKEN = "8311901559:AAEJIF3HbxtkVnf8YjVYQ5IBgAfcPl9Axh4"
+TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 @app.post("/hello")
 async def webhook(request : Request):
     data = await request.json()
-    print(data)
-    return "Hello There, from fastAPI"
+    print("Update received:", data)
+    message = data.get("message",{})
+    chat_id = message.get("chat",{}).get("id")
+    text = message.get("text","")
+
+    if chat_id:
+        reply_text = f"You Said : {text}"
+        async with httpx.AsyncClient as client:
+            await client.post(
+                f"{TELEGRAM_API}/sendMessage",
+                json={"chat_id":chat_id, "text":reply_text}
+                
+            )
+    return {"ok": True}
